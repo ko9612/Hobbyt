9bc516a357ee8ade38a31b6d57c70cf132a67a9f
JunginPark4364 <wjddlsee@gmail.com>
2023-01-01 21:36:13 +0900

    feat: 주문 관련 엔티티 추가 #18

---
+++ server/src/main/java/com/hobbyt/domain/entity/Order.java
@@ -0,0 +1,43 @@
+package com.hobbyt.domain.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.hobbyt.global.entity.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private OrderStatus status;
+
+	@OneToMany(mappedBy = "order")
+	List<OrderItem> orderItems = new ArrayList<>();
+}
