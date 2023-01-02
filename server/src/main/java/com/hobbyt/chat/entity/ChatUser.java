6009adffef3f60378a230f2f76938aa0e17053d4
toneofrain <toneofrain@gmail.com>
2023-01-02 18:01:47 +0900

    feat: ChatUser mapping (#26)

--- server/src/main/java/com/hobbyt/chat/entity/ChatUser.java
+++ server/src/main/java/com/hobbyt/chat/entity/ChatUser.java
@@ -1,10 +1,14 @@
 package com.hobbyt.chat.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.hobbyt.domain.entity.User;
 import com.hobbyt.global.entity.BaseEntity;
 
 import lombok.AccessLevel;
@@ -16,4 +20,10 @@ public class ChatUser extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chatroom_id")
+	private ChatRoom chatRoom;
 }
