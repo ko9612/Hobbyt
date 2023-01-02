cd7361e125e75094c52f46e3e18ccf44bc4b0e31
조성호 saintho <toneofrain@gmail.com>
2023-01-02 17:40:27 +0900

    Feat/seongho/post #25
    * feat: Post Entity (#25)
    
    * feat: Tag Entity (#25)
    
    * feat: PostTag Entity (#25)
    
    * feat: PostComment Entity (#25)
    
    * feat: PostLike Entity (#25)
    
    * feat: Post User Mapping (#25)
    
    * feat: Post PostComment Mapping (#25)
    
    * feat: Post PostLike Mapping (#25)
    
    * feat: Post PostTag Mapping (#25)

---
+++ server/src/main/java/com/hobbyt/post/entity/PostTag.java
@@ -0,0 +1,28 @@
+package com.hobbyt.post.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.hobbyt.global.entity.Tag;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostTag {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tag_id")
+	private Tag tag;
+}
