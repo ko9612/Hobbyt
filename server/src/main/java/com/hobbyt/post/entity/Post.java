1f1121e450e52cecf49a775e6a619c634b22a4ab
toneofrain <toneofrain@gmail.com>
2023-01-02 17:28:41 +0900

    feat: Post User Mapping (#25)

--- server/src/main/java/com/hobbyt/post/entity/Post.java
+++ server/src/main/java/com/hobbyt/post/entity/Post.java
@@ -1,19 +1,28 @@
 package com.hobbyt.post.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.hobbyt.domain.entity.User;
 import com.hobbyt.global.entity.Article;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Post extends Article {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "writer_id")
+	private User user;
 }
