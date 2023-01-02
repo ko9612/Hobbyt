759200624f3c90ddaab7198212c8e5eaf6fa1be8
JunginPark4364 <wjddlsee@gmail.com>
2023-01-02 14:27:00 +0900

    refactor: 인증코드 생성 메서드 분리 #23

--- server/src/main/java/com/hobbyt/domain/user/service/AuthenticationCode.java
+++ server/src/main/java/com/hobbyt/domain/user/service/AuthenticationCode.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 public class AuthenticationCode {
 	private static final int CODE_COUNT = 8;
 	private static final int NUMBER_OF_CASES = 3;
+	private static final int LOWER_CASE = 0;
+	private static final int UPPER_CASE = 1;
+	private static final int NUMBER = 2;
 
 	private final String code;
 
@@ -20,20 +23,24 @@ public class AuthenticationCode {
 		Random random = new Random();
 
 		int index = 0;
-		while (index < CODE_COUNT) {
-			switch (random.nextInt(NUMBER_OF_CASES)) {
-				case 0:    // a~z (ex. 1+97=98 => (char)98 = 'b')
-					code.append((char)((random.nextInt(26)) + 97));
-					break;
-				case 1:    // A ~ Z
-					code.append((char)((random.nextInt(26)) + 65));
-					break;
-				case 2:    // 0 ~ 9
-					code.append(random.nextInt(10));
-					break;
-			}
+		while (index++ < CODE_COUNT) {
+			addNextCode(code, random);
 		}
 
 		return code.toString();
+	}
+
+	private void addNextCode(StringBuffer code, Random random) {
+		switch (random.nextInt(NUMBER_OF_CASES)) {
+			case LOWER_CASE:    // a~z (ex. 1+97=98 => (char)98 = 'b')
+				code.append((char)((random.nextInt(26)) + 97));
+				break;
+			case UPPER_CASE:    // A ~ Z
+				code.append((char)((random.nextInt(26)) + 65));
+				break;
+			case NUMBER:    // 0 ~ 9
+				code.append(random.nextInt(10));
+				break;
+		}
 	}
 }
