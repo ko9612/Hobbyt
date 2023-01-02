204d455d1bfecd8de10069e232c6e31323e27007
JunginPark4364 <wjddlsee@gmail.com>
2023-01-02 23:22:15 +0900

    refactor: email 전달 방법 수정 #23

--- server/src/main/java/com/hobbyt/domain/user/controller/AuthController.java
+++ server/src/main/java/com/hobbyt/domain/user/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hobbyt.domain.user.service.AuthService;
@@ -11,13 +12,13 @@ import com.hobbyt.domain.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthService authService;
 
-	@PostMapping("/confirm")
-	public ResponseEntity mailConfirm(String email) {
+	@PostMapping("/code")
+	public ResponseEntity mailConfirm(@RequestParam String email) {
 		String code = authService.sendAuthenticationCodeEmail(email);
 
 		return new ResponseEntity(code, HttpStatus.CREATED);
