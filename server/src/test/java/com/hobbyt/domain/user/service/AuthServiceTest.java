9724bfe76ff0ce905709beed4014b11fc8530c06
JunginPark4364 <wjddlsee@gmail.com>
2023-01-04 02:42:09 +0900

    fix: 테스트 코드 수정 #23

--- server/src/test/java/com/hobbyt/domain/user/service/AuthServiceTest.java
+++ server/src/test/java/com/hobbyt/domain/user/service/AuthServiceTest.java
@@ -1,12 +1,9 @@
 package com.hobbyt.domain.user.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.regex.Pattern;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,13 +12,11 @@ import com.hobbyt.domain.user.dto.request.EmailRequest;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
-	private static final String CODE_PATTERN = "[0-9a-zA-Z]{8}";    // 0-9 or a-z or A-Z 로 이루어진 8자리
-
 	@Mock
 	private MailService mailService;
 
 	@Mock
-	private MailContentBuilder contentBuilder;
+	private MailContentBuilder mailContentBuilder;
 
 	@InjectMocks
 	private AuthService authService;
@@ -30,13 +25,14 @@ class AuthServiceTest {
 	@Test
 	void sendAuthenticationCodeEmail() {
 		//given
+		String code = AuthenticationCode.createCode().getCode();
 		EmailRequest emailRequest = new EmailRequest("test@gmail.com");
 
 		//when
-		String code = authService.sendAuthenticationCodeEmail(emailRequest);
+		String result = authService.sendAuthenticationCodeEmail(emailRequest);
 
 		//then
-		assertThat(code.length()).isEqualTo(8);
-		assertThat(Pattern.matches(CODE_PATTERN, code)).isTrue();
+		BDDMockito.then(mailService).should().sendMail(BDDMockito.any(NotificationEmail.class));
+		BDDMockito.then(mailContentBuilder).should().build(BDDMockito.anyString(), BDDMockito.anyMap());
 	}
 }
