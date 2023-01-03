5bf58710bbf3406a2ca533b0088c49f361b62f5f
JunginPark4364 <wjddlsee@gmail.com>
2023-01-04 02:36:58 +0900

    refactor: 객체 생성방법 변경 #23

--- server/src/main/java/com/hobbyt/domain/user/service/NotificationEmail.java
+++ server/src/main/java/com/hobbyt/domain/user/service/NotificationEmail.java
@@ -1,12 +1,17 @@
 package com.hobbyt.domain.user.service;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationEmail {
 	private String receiver;
 	private String subject;
 	private String content;
+
+	public static NotificationEmail of(String receiver, String subject, String content) {
+		return new NotificationEmail(receiver, subject, content);
+	}
 }
