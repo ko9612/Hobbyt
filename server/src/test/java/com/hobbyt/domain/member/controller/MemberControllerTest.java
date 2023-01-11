83411b93984acc1fbd3d361f137ab13728cf7b03
JunginPark4364 <wjddlsee@gmail.com>
2023-01-11 17:15:40 +0900

    test: 컨트롤러 계층 회원정보 변경 테스트 추가 #58

--- server/src/test/java/com/hobbyt/domain/member/controller/MemberControllerTest.java
+++ server/src/test/java/com/hobbyt/domain/member/controller/MemberControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hobbyt.config.TestMemberDetailService;
 import com.hobbyt.domain.member.dto.request.SignupRequest;
+import com.hobbyt.domain.member.dto.request.UpdateMemberRequest;
+import com.hobbyt.domain.member.dto.response.UpdateMemberResponse;
 import com.hobbyt.domain.member.service.MemberService;
 import com.hobbyt.global.security.jwt.JwtTokenProvider;
 import com.hobbyt.global.security.member.MemberDetails;
@@ -39,6 +42,15 @@ class MemberControllerTest {
 
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
+
+	private String accessToken;
+	private MemberDetails memberDetails;
+
+	@BeforeEach
+	void setup() {
+		accessToken = jwtTokenProvider.createAccessToken(EMAIL, USER_AUTHORITY);
+		memberDetails = dummyMemberDetails(1L, NICKNAME, EMAIL, PASSWORD);
+	}
 
 	@DisplayName("정상 회원가입 api")
 	@Test
@@ -76,4 +88,35 @@ class MemberControllerTest {
 		actions.andExpect(status().isOk())
 			.andDo(print());
 	}
+
+	@DisplayName("회원정보 변경")
+	@Test
+	void update() throws Exception {
+		UpdateMemberRequest request = dummyUpdateMemberRequest(NICKNAME, DESCRIPTION, PHONE_NUMBER, ZIPCODE,
+			STREET, DETAIL, BANK, ACCOUNT_NUMBER);
+		UpdateMemberResponse response = dummyUpdateMemberResponse(NICKNAME, DESCRIPTION, PHONE_NUMBER,
+			ZIPCODE, STREET, DETAIL, BANK, ACCOUNT_NUMBER);
+		given(memberService.update(any(MemberDetails.class), any(UpdateMemberRequest.class)))
+			.willReturn(response);
+
+		ResultActions actions = mockMvc.perform(patch("/api/members/myPage")
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON)
+			.header(AUTH_HEADER, TOKEN_TYPE + " " + accessToken)
+			.content(objectMapper.writeValueAsString(request))
+		);
+
+		actions.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.nickname").value(NICKNAME))
+			.andExpect(jsonPath("$.description").value(DESCRIPTION))
+			.andExpect(jsonPath("$.phoneNumber").value(PHONE_NUMBER))
+			.andExpect(jsonPath("$.address.zipcode").value(ZIPCODE))
+			.andExpect(jsonPath("$.address.street").value(STREET))
+			.andExpect(jsonPath("$.address.detail").value(DETAIL))
+			.andExpect(jsonPath("$.account.bank").value(BANK))
+			.andExpect(jsonPath("$.account.number").value(ACCOUNT_NUMBER))
+			.andDo(print());
+	}
+
 }
