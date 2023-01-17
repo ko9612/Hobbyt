d7aab787c870aa8a80170afed285a5ff1ed77074
JunginPark4364 <wjddlsee@gmail.com>
2023-01-17 16:16:33 +0900

    refactor: 판매게시글 엔티티 위치 변경 #70

--- server/src/main/java/com/hobbyt/domain/sale/entity/Sale.java
+++ server/src/main/java/com/hobbyt/domain/sale/entity/Sale.java
@@ -1,4 +1,7 @@
-package com.hobbyt.domain.entity;
+package com.hobbyt.domain.sale.entity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -10,6 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.hobbyt.domain.entity.Account;
+import com.hobbyt.domain.entity.Period;
+import com.hobbyt.domain.entity.Product;
 import com.hobbyt.domain.member.entity.Member;
 import com.hobbyt.global.entity.Article;
 
@@ -28,13 +34,25 @@ public class Sale extends Article {
 	@JoinColumn(name = "writer_id")
 	private Member writer;
 
+	// 환불/교환안내
 	@Column(nullable = false, columnDefinition = "MEDIUMTEXT")
 	private String refundPolicy;
 
+	// 판매기간 연,월,일
 	@Embedded
 	@Column(nullable = false)
 	private Period period;
 
+	// 입금계좌 정보
 	@Column(nullable = false)
 	private Account account;
+
+	// 제작과정 url
+	private String url;
+
+	// 주의 사항
+	// 상시판매 여부
+	// 배송정보	>> 평균 소요일, 배송사, 가격
+
+	List<Product> products = new ArrayList<>();
 }
