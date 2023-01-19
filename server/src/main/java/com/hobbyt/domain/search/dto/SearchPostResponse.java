1dabaae0cef4fc5e62bdbb7ae5a632d180a04cb4
toneofrain <toneofrain@gmail.com>
2023-01-20 03:18:44 +0900

    feat: 블로그 검색 dto 구현 (#64)

--- server/src/main/java/com/hobbyt/domain/search/dto/SearchPostResponse.java
+++ server/src/main/java/com/hobbyt/domain/search/dto/SearchPostResponse.java
@@ -1,12 +1,19 @@
 package com.hobbyt.domain.search.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Getter;
 
 @Getter
 public class SearchPostResponse {
 	private Boolean hasNext;
+	private List<PostCard> posts;
+
+	public SearchPostResponse(Boolean hasNext, List<PostCard> posts) {
+		this.hasNext = hasNext;
+		this.posts = posts;
+	}
 
 	public static class PostCard {
 		private Long id;
