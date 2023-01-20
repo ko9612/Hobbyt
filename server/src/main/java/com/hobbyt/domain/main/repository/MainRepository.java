b37c6331deeabe32eb2b7d1f1a43e4c390ab4b3e
toneofrain <toneofrain@gmail.com>
2023-01-20 09:51:34 +0900

    feat: 인기 블로그 게시물 쿼리 작성 (#64)

--- server/src/main/java/com/hobbyt/domain/main/repository/MainRepository.java
+++ server/src/main/java/com/hobbyt/domain/main/repository/MainRepository.java
@@ -1,4 +1,9 @@
 package com.hobbyt.domain.main.repository;
 
+import java.util.List;
+
+import com.hobbyt.domain.main.entity.HotPost;
+
 public interface MainRepository {
+	List<HotPost> getHotPost();
 }
