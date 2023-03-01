129a578cfea9aa45709d492907db575b5c678408
ko9612 <kohana735@gmail.com>
2023-03-01 16:49:42 +0900

    fix: 알람 수정

--- client/src/components/Websoket/socketjs.tsx
+++ client/src/components/Websoket/socketjs.tsx
@@ -28,7 +28,7 @@ export default function WebSoketConnect(isLogin, userId) {
         console.log("beforeConnect");
       },
       connectHeaders: {
-        Authorization: `${token}`,
+        Authorization: `Bearer ${token}`,
       },
       debug(str) {
         console.log(`str`, str);
